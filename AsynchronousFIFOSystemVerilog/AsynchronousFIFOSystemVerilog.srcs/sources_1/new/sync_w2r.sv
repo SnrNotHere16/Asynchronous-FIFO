96bdfdcc18aefa6dc5084d86dc6b279d39d69c31
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-07 11:55:50 -0800

    added files

---
+++ AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/sync_w2r.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/07/2022 11:53:55 AM
+// Design Name: 
+// Module Name: sync_w2r
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sync_w2r(
+
+    );
+endmodule
