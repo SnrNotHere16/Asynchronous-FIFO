22b63b8b123594cf9c1dbeddb4c235f6eb97bc67
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-08 11:55:03 -0800

    Update sync_r2w.sv
    finished sync_r2w.sv

--- AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/sync_r2w.sv
+++ AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/sync_r2w.sv
@@ -1,4 +1,16 @@
-module sync_r2w (); 
+module sync_r2w #(parameter ADDRSIZE = 4)
+				 (fifo1Inter sync_r2w_mod); 
+				 
+				 logic [ADDRSIZE: 0] wq1_rptr; 
+				 
+				 always_ff @(posedge sync_r2w_mod.wclk, posedge sync_r2w_mod.wrst_n) begin 
+					if (!sync_r2w_mod.wrst_n) begin 
+						{sync_r2w_mod.wq2_rptr, wq1_rptr} <= 0;
+					end 
+					else begin 
+						{sync_r2w_mod.wq2_rptr, wq1_rptr} <= {wq1_rptr, sync_r2w_mod.rptr};
+					end 
+				 end 
 
 
 
