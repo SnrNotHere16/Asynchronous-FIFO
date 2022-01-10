ba1afba1a6a6a1e66ecb7b59386b3e946b00be66
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-10 11:28:54 -0800

    AsynchrnousFIFO works
    FIFO1.sv works. It has been tested and verified on a testbench.

--- AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sim_1/new/AsynchronousFIFOTB.sv
+++ AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sim_1/new/AsynchronousFIFOTB.sv
@@ -1,8 +1,92 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-
+//  Dual-clock asynchronous FIFO design and testbench in SystemVerilog
+//  Based on Cliff Cumming's Simulation and Synthesis Techniques for Asynchronous FIFO Design
+//  http://www.sunburst-design.com/papers/CummingsSNUG2002SJ_FIFO1.pdf
+//  Copyright (C) 2015 Jason Yu (http://www.verilogpro.com)
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module AsynchronousFIFOTB();
+parameter DSIZE = 8;
+  parameter ASIZE = 4;
+
+  logic [DSIZE-1:0] rdata;
+  logic wfull;
+  logic rempty;
+  logic [DSIZE-1:0] wdata;
+  logic winc, wclk, wrst_n;
+  logic rinc, rclk, rrst_n;
+
+  // Model a queue for checking data
+  logic [DSIZE-1:0] verif_data_q[$];
+  logic [DSIZE-1:0] verif_wdata;
+
+
+  // Instantiate the FIFO
+  fifo1 #(DSIZE, ASIZE) dut (.*);
+
+  initial begin
+    wclk = 1'b0;
+    rclk = 1'b0;
+
+    fork
+      forever #10ns wclk = ~wclk;
+      forever #35ns rclk = ~rclk;
+    join
+  end
+
+  initial begin
+    winc = 1'b0;
+    wdata = '0;
+    wrst_n = 1'b0;
+    repeat(5) @(posedge wclk);
+    wrst_n = 1'b1;
+
+    for (int iter=0; iter<2; iter++) begin
+      for (int i=0; i<32; i++) begin
+        @(posedge wclk iff !wfull);
+        winc = (i%2 == 0)? 1'b1 : 1'b0;
+        if (winc) begin
+          wdata = $urandom;
+          verif_data_q.push_front(wdata);
+        end
+      end
+      #1us;
+    end
+  end
+
+  initial begin
+    rinc = 1'b0;
+
+    rrst_n = 1'b0;
+    repeat(8) @(posedge rclk);
+    rrst_n = 1'b1;
+
+    for (int iter=0; iter<2; iter++) begin
+      for (int i=0; i<32; i++) begin
+        @(posedge rclk iff !rempty)
+        rinc = (i%2 == 0)? 1'b1 : 1'b0;
+        if (rinc) begin
+          verif_wdata = verif_data_q.pop_back();
+          // Check the rdata against modeled wdata
+          $display("Checking rdata: expected wdata = %h, rdata = %h", verif_wdata, rdata);
+          assert(rdata === verif_wdata) else $error("Checking failed: expected wdata = %h, rdata = %h", verif_wdata, rdata);
+        end
+      end
+      #1us;
+    end
+
+    $finish;
+  end
 endmodule
--- AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/fifo1.sv
+++ AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/fifo1.sv
@@ -1,3 +1,6 @@
+
+//Inteface of the asynchronous FIFO, including the modports of all 
+//its modules 
 interface fifo1Inter #(parameter DSIZE = 8, parameter ASIZE = 4)
     ( output var logic [DSIZE-1:0] rdata, 
       output var logic wfull, rempty, 
@@ -45,15 +48,15 @@ module fifo1 #(parameter DSIZE = 8,
 							   .wdata(wdata), .wclken(winc), .winc(winc), .wclk(wclk), .wrst_n(wrst_n), 
 							   .rinc(rinc), .rclk(rclk), .rrst_n(rrst_n)); 
 	
-	sync_r2w sync_r2w (.r2w(fifo1Interface.sync_r2w_mod));
+	sync_r2w sync_r2w (.sync_r2w_mod(fifo1Interface.sync_r2w_mod));
 	
-	sync_w2r  sync_w2r (.w2r(fifo1Interface.sync_w2r_mod)); 
+	sync_w2r  sync_w2r (.sync_w2r_mod(fifo1Interface.sync_w2r_mod)); 
 	
 	
-	fifomem   fifomem(.fifo(fifo1Interface.fifomem_mod));
+	fifomem   fifomem(.fifomem_mod(fifo1Interface.fifomem_mod));
 
-	rptr_empty rptr_empty(.rptr_e(fifo1Interface.rptr_empty_mod)); 
+	rptr_empty rptr_empty(.rptr_empty_mod(fifo1Interface.rptr_empty_mod)); 
 	
-	wptr_full wptr_full (.wptr_f(fifo1Interface.wptr_full_mod)); 
+	wptr_full wptr_full (.wptr_full_mod(fifo1Interface.wptr_full_mod)); 
 
 endmodule: fifo1 
