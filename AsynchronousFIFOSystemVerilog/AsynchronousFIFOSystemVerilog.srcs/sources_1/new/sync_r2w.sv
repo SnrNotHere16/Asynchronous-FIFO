61e222853d352300d60674dee8a2214e66423892
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-10 11:44:40 -0800

    Update sync_r2w.sv
    Finished commenting sync_r2w.sv

--- AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/sync_r2w.sv
+++ AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/sync_r2w.sv
@@ -1,3 +1,20 @@
+/*
+File: sync_r2w.sv - read-domain to write-domain synchronizer 
+	This a synchronizer module that is used to synchronize the read pointer into the write-clock domain. 
+ The synchronized read pointer will be used by the wptr_full module to generate the FIFO full condition. 
+ This module only contains flip-flops that are synchronized to the real clock. No other logic is 
+ included in this module. 
+ 
+	This is a simple synchronizer module, used to pass an n-bit pointer from the read clock domain 
+ to the write clock domain, through a pair of registers that are clocked by the FIFO write clock. 
+ Notice the simplicity of the always block that concatenates the two registers together for reset 
+ and shifting. The synchronizer always block is only three lines of code. 
+ 
+ All module outputs are registerd for simplified synthesis using time budgeting. All outputs of 
+ this module are entirely synchronous to the wclk and all asynchronous inputs to this module are 
+ from the rclk domain with all signals named using an "r" prefix, making it easy to set a false path 
+ on all "r*" signlas for simplified static timing analysis. 
+*/
 module sync_r2w #(parameter ADDRSIZE = 4)
 				 (fifo1Inter sync_r2w_mod); 
 				 
