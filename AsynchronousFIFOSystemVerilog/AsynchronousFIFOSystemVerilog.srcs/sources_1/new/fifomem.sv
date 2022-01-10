3c33b6d801775ff43aea6bc416dcf98815f2ba40
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-10 12:20:07 -0800

    Update fifomem.sv
    Finished commenting the file above.

--- AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/fifomem.sv
+++ AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/fifomem.sv
@@ -1,3 +1,20 @@
+/*
+File: fifomem.sv - FIFO memory buffer 
+	This is the FIFO memory buffer that is accessed by both the read write and read clock domains. 
+This buffer is most likely an instantiated, synchronous dual-port RAM. Other memory styles can 
+be adapted to functions as the FIFO buffer. 
+
+	The FIFO memory buffer is typically an instantiated ASIC or FPGA dual-port, synchronous memory
+device. The memory buffer could also be synthesized to ASIC or FPGA registers using RTL code in
+this module. 
+
+About an instantiated vendor RAM versus a Verilog-declared RAM, the Synopsys DesignWare team did 
+internal analysis and found that for sizes up to 256 bits, there is no lost area or performance 
+using the Verilog-declared RAM compared to an instantiated vendor RAM. 
+
+If a vendor RAM is instatiated, it is highly recommended that the instantiation be done using 
+named port connections. 
+*/
 module fifomem #(parameter DATASIZE = 8, 
 				 parameter ADDRSIZE = 4)
 				 (fifo1Inter fifomem_mod);
