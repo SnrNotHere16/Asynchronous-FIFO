fc33a422cab894ffb24a9b54ee6592278803b46a
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-10 12:13:59 -0800

    Update rptr_empty.sv
    Finished commenting this file

--- AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/rptr_empty.sv
+++ AsynchronousFIFOSystemVerilog/AsynchronousFIFOSystemVerilog.srcs/sources_1/new/rptr_empty.sv
@@ -1,3 +1,18 @@
+/*
+File: rptr_empty.sv - Read pointer & empty generation logic
+	This module is completely synchronous to the read-clock domain and contains the FIFO read pointer 
+and empty-flag logic. 
+
+	This module encloses all of the FIFO logic that is generated within the read clock domain (except 
+synchronizers). The read pointer is a dual n-bit Gray code counter. The n-bit pointer(rptr) is passed 
+to the write clock domain through the sync_r2w module. The (n-1)-bit pointer (raddr) is used to 
+address the FIFO buffer. 
+
+The FIFO empty output is registered and is asserted on the next rising rclk edge when the next rptr
+value equals the synchronized wprt value. All module outputs are registerd for simplified synthesis
+using time budgeting. This module is entirely synchronous to the rclk for simplified static timing 
+analysis. 
+*/
 module rptr_empty #(parameter ADDRSIZE = 4) 
 					(fifo1Inter rptr_empty_mod); 
 					
