521ef0e07673dd53e81ea1f4cb70737718a2c08f
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-22 15:11:01 -0800

    Update TestbenchTop.sv

--- AsynchronousFIFOSystemVerilog/UVMTestBench/TestbenchTop.sv
+++ AsynchronousFIFOSystemVerilog/UVMTestBench/TestbenchTop.sv
@@ -7,11 +7,13 @@ endinterface: dut_if
 
 module TestBenchTop (); 
 
+	//import uvm_pkg::*; 
+	//import my_pkg::*;
 	dut_if _if(); 
 	AsynchronousFIFO_dut dut (.dut_inter(_if.dut));
 
 	initial begin 
-		//run_test(); 
+		//run_test(); //name of the test class 
 	end 
 
 endmodule: TestBenchTop
