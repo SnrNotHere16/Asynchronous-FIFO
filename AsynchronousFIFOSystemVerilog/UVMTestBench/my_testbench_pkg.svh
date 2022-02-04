f08049dca02576485d82269c51027d6ac39abd26
SnrNotHere16 <aquineda16@hotmail.com>
2022-02-03 21:06:13 -0800

    Update my_testbench_pkg.svh
    Finished defining my_test class

--- AsynchronousFIFOSystemVerilog/UVMTestBench/my_testbench_pkg.svh
+++ AsynchronousFIFOSystemVerilog/UVMTestBench/my_testbench_pkg.svh
@@ -42,8 +42,19 @@ package  my_testbench_pkg;
 		function new(string name, uvm_component parent);
 			super.new(name, parent);
 		endfunction: new 
-
-	
+		
+		function void build_phase(uvm_phase phase); 
+			env = my_env::type_id::create("env", this); 
+		endfunction: build_phase 
+		
+		task run_phase(uvm_phase phase); 
+			//We raise objection to keep the test from completing 
+			phase.raise_objection(this); 
+			#10; 
+			`uvm_warning("", "Hello World!")
+			//We drop objection to allow the test to complete 
+			phase.drop_objection(this); 
+		endtask: run_phase
 	endclass: my_test 
 
 
