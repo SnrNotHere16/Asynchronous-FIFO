ccf665541d0d14385cfbf4796defc207ae9278ca
SnrNotHere16 <aquineda16@hotmail.com>
2022-02-13 20:33:24 -0800

    Update my_sequence.svh
    added two  new sequences

--- UVMExamples/Two/FullyWrite&Read/my_sequence.svh
+++ UVMExamples/Two/FullyWrite&Read/my_sequence.svh
@@ -12,6 +12,41 @@ class my_transaction extends uvm_sequence_item;
 endclass: my_transaction 
 
 
+class read_sequence extends uvm_sequence #(my_transaction); 
+	`uvm_object_utils(read_sequence)
+	
+	function new (string name = ""); 
+		super.new(name); 
+	endfunction: new 
+
+	task body;
+		repeat (1) begin 
+			req = my_transaction::type_id::create("req");
+			start_item(req); 
+			assert (req.randomize with {winc == 0; rinc == 1; wdata <= 255;});
+			finish_item(req); 
+		end 
+	endtask: body 
+endclass: read_sequence 
+
+class full_write_sequence extends uvm_sequence#(my_transaction); 
+	`uvm_object_utils(full_write_sequence) 
+	
+	function new (string name = ""); 
+		super.new(name); 
+	endfunction: new 
+
+	task body; 
+		repeat (17) begin
+			req = my_transaction::type_id::create("req"); 
+			start_item(req); 
+			assert(req.randomize with {winc == 1; rinc == 0; wdata <= 255;})
+			finish_item(req); 
+		end
+	endtask: body 
+
+endclass: full_write_sequence
+
 class my_sequence extends uvm_sequence #(my_transaction);
 	`uvm_object_utils(my_sequence)
 	
