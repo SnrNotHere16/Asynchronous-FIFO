cd1a04b70de746660bfef0f37b64076f2222a01b
SnrNotHere16 <aquineda16@hotmail.com>
2022-02-14 13:43:00 -0800

    Update my_sequence.svh

--- UVMExamples/Two/FullWrite&ReadEmpty/my_sequence.svh
+++ UVMExamples/Two/FullWrite&ReadEmpty/my_sequence.svh
@@ -20,13 +20,22 @@ class my_sequence extends uvm_sequence #(my_transaction);
 	endfunction: new 
 
 	task body; 
-		repeat(8) begin
+		int count = 0; 
+		repeat(38) begin
+		count++; 
 			req = my_transaction::type_id::create("req");
             start_item(req);
-
-           if (!req.randomize()) begin
+		if (count < 17) begin 
+           if (!req.randomize() with {winc == 1; rinc == 0; wdata <= 255;}) begin
            `uvm_error("MY_SEQUENCE", "Randomize failed.");
            end
+		 end 
+		 
+		 else begin 
+			if (!req.randomize() with {winc == 1; rinc == 0; wdata <= 255;}) begin 
+				`uvm_error("MY_SEQUENCE", "Randomize failed")
+			end 
+		 end 
 		   
 		    finish_item(req);
 	end 
