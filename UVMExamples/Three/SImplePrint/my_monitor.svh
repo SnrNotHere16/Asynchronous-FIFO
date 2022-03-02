d783b5309f1822711c836aaa8f48c9be58af3b7a
SnrNotHere16 <aquineda16@hotmail.com>
2022-03-02 13:10:45 -0800

    Update my_monitor.svh

---
+++ UVMExamples/Three/SImplePrint/my_monitor.svh
@@ -0,0 +1,10 @@
+class my_monitor extends uvm_monitor #(my_transaction); 
+	`uvm_component_utils(my_monitor) 
+	
+	function new (string name = "my_monitor", uvm_component parent = null); 
+		super.new(name, parent); 
+	endclass: new 
+
+
+
+endclass: my_monitor
