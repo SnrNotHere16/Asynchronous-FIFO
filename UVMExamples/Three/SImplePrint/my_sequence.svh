98dbde268b981c83b3434ec90bad1d284a265e7e
SnrNotHere16 <aquineda16@hotmail.com>
2022-03-02 13:18:05 -0800

    Update my_sequence.svh
    Added the outputs of the fifo to the transaction object

--- UVMExamples/Three/SImplePrint/my_sequence.svh
+++ UVMExamples/Three/SImplePrint/my_sequence.svh
@@ -1,10 +1,17 @@
 class my_transaction extends uvm_sequence_item; 
 	`uvm_object_utils(my_transaction) 
-	 rand logic [7:0] wdata; 
-	 rand logic winc; 
-	 rand logic rinc; 
-	 
-	
+	`define DSIZE 8
+	//inputs 
+		rand logic [DSIZE:0] wdata; 
+		rand logic winc; 
+		rand logic rinc; 
+	//outputs 
+		logic [DSIZE-1:0] rdata; 
+		logic wfull; 
+		logic rempty; 
+		logic wclk, wrst_n; 
+		logic rclk, rrst_n; 
+		
 	function new (string name = "");
 		super.new(name); 
 	endfunction: new 
