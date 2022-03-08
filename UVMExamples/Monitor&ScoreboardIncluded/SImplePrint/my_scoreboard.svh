8419b18ded705432279ceca6ec4708b37019796c
SnrNotHere16 <aquineda16@hotmail.com>
2022-03-07 22:14:19 -0800

    Added first covergroup

--- UVMExamples/Monitor&ScoreboardIncluded/SImplePrint/my_scoreboard.svh
+++ UVMExamples/Monitor&ScoreboardIncluded/SImplePrint/my_scoreboard.svh
@@ -2,9 +2,15 @@ class my_scoreboard extends uvm_scoreboard;
 	`uvm_component_utils(my_scoreboard) 
 	
 	uvm_analysis_imp #(my_transaction, my_scoreboard) m_analysis_imp; 
-
-	function new (string name = "my_scoreboard", uvm_component parent); 
-		super.new(name, parent); 
+	my_transaction item_s; 
+	
+	covergroup my_transaction_c; 
+		T: coverpoint item_s.winc; 
+	endgroup: my_transaction_c
+	
+	function new (string name = "my_scoreboard", uvm_component parent);  
+		super.new(name, parent);
+		my_transaction_c = new(); 
 	endfunction: new 
 	
 	function void build_phase(uvm_phase phase); 
@@ -13,6 +19,9 @@ class my_scoreboard extends uvm_scoreboard;
 	endfunction: build_phase
 
 	function write (my_transaction item); 
+			item_s = item; 
+			my_transaction_c.sample(); 
+          //$display("Coverage = %f", my_transaction_c.get_coverage());
 			`uvm_info("Monitor:Write", item.convert2string(), UVM_LOW)
 	endfunction: write 
 	
